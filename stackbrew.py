0bc74cc17928fd828f4b271770c02073478d4652
Solomon Hykes <solomon@dotcloud.com>
2012-01-28 22:22:30 -0800

    Experimental support for heroku buildpacks. Only bin/compile is recognized for now

--- stackbrew.py
+++ stackbrew.py
@@ -1,132 +1,65 @@
 #!/usr/bin/env python
 
-
 import os
-from os import chdir, listdir
 import sys
-import shutil
-from shutil import copytree, rmtree, copy
-import subprocess
-from time import strftime
-from random import random
-from multiprocessing import Process
-
 import yaml
+import shutil
+import subprocess
 
 
-def log(msg):
-    print "----- " + msg
-
-def call(cmd):
-    log(cmd)
-    return subprocess.call(cmd, shell=True)
-
-def fork(fn):
-    def wrapper(*args, **kwargs):
-        p = Process(target=fn, args=args, kwargs=kwargs)
-        p.start()
-        p.join()
-    return wrapper
-
-def detect_services(path):
-    path = os.path.abspath(path)
-    if not os.path.exists(path):
-        raise IOError("No such file or directory: '{path}'".format(path=path))
+def load_stack(app_dir):
+    """ Load stack description from an app's directory """
     for filename in ['Stackfile', 'stackfile', 'dotcloud.yml', 'dotcloud_build.yml']:
-        filepath = os.path.join(path, filename)
+        filepath = os.path.join(app_dir, filename)
         if os.path.exists(filepath):
-            return yaml.load(file(filepath))
-    return { "main": {} }
-
-def get_root(app):
-    path = os.path.expanduser("~/.stackbrew/{app}".format(app=app))
-    return "{path}".format(
-        path            = path
-    )
-
-def get_src(service):
-    return os.path.join(os.environ.get("SERVICES_LIBRARY", os.path.join(os.path.dirname(__file__), "apps")), service)
-
-def get_build(app, service=None):
-    return "{root}/build/{service}".format(root=get_root(app), service = service or "")
-
-def do_build():
-    app = os.path.basename(os.getcwd())
-    if os.path.exists(get_root(app)):
-        print "Build already exists: {root}".format(root=get_root(app))
-        sys.exit(1)
-    build = get_build(app)
-    if os.path.exists(build):
-        print "removing {build}".format(build=build)
-    src = "."
-    for (name, settings) in detect_services(src).items():
-        build_service(
-            name    = name,
-            source  = src,
-            dest    = "{build}/{service}".format(build=build, service=name),
-            **settings
-        )
-
-def do_run(app, service, *cmd):
-    with pushdir(get_build(app, service)):
-        subprocess.call("[ -e profile ] && . profile; {cmd}".format(cmd=" ".join(cmd)),
-                shell=True,
-                env=dict(os.environ,
-                        PATH=".:" + os.environ["PATH"],
-                        HOME=".")
+            return dict(
+                (key, value if type(value) == str else value.get("type"))
+                for key, value in yaml.load(file(filepath)).items()
             )
 
 
+def get_buildpack_dir(buildpack):
+    if os.path.exists(buildpack):
+        return buildpack
+    for buildpack_dir in os.environ.get("BUILDPACK_PATH", "").split(":"):
+        path = os.path.join(buildpack_dir, buildpack)
+        print "Checking for {path}".format(path=path)
+        if os.path.exists(path):
+            return path
+    raise IOError("No such file or directory: {buildpack}".format(buildpack=buildpack))
+
+
+def build_service(service_name, build_dir, buildpack):
+    """ Build the service in-place at `build_dir` using `buildpack`.
+        `service_name` is provided as a convenience.
+    """
+    buildpack_dir = get_buildpack_dir(buildpack)
+    cache_dir = "{buildpack}/_cache".format(buildpack=buildpack_dir)
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    print "['{build_dir}'] building service '{service_name}' with buildpack '{buildpack}' and cache '{cache_dir}'".format(**locals())
+    subprocess.call(["{buildpack}/bin/compile".format(buildpack=buildpack_dir), build_dir, cache_dir])
+
+
+def copy(src, dst):
+    print "Copying {src} to {dst}".format(**locals())
+    shutil.copytree(src, dst)
+
+
+def build_app(source_dir, build_dir):
+    """ Build the application source code at `source_dir` into `build_dir`.
+        The build will fail if build_dir alread exists.
+    """
+    os.makedirs(build_dir)
+    for (service, buildpack) in load_stack(source_dir).items():
+        print "{service} -> {buildpack}".format(service=service, buildpack=buildpack)
+        service_build_dir = "{build_dir}/{service}".format(**locals())
+        copy(source_dir, service_build_dir)
+        build_service(service, service_build_dir, buildpack)
+
+
 def main():
-    eval("do_{cmd}".format(cmd=sys.argv[1]))(*sys.argv[2:])
-
-
-def execute_script(path, home):
-    """ Execute `path`, if it exists, with HOME set to `home`. """
-    if os.path.exists(path):
-        os.environ["OLDHOME"] = os.environ["HOME"]
-        os.environ["HOME"] = home 
-        log("Executing {path:<30} in {cwd}".format(path=path, cwd=os.getcwd()))
-        subprocess.call(path)
-        os.environ["HOME"] = os.environ["OLDHOME"]
-
-
-class pushdir:
-    def __init__(self, dir):
-        self.dir = dir
-    def __enter__(self):
-        self.olddir = os.getcwd()
-        os.chdir(self.dir)
-    def __exit__(self, *args):
-        os.chdir(self.olddir)
-
-
-
-def build_service(name, source, dest, type=None, **settings):
-    source = os.path.abspath(source)
-    sources = [source]
-    dest = os.path.abspath(dest)
-    log("Building  {name:<30} to {dest}".format(name=name, dest=dest))
-    # Move to the code directory
-    with pushdir(source):
-        with pushdir(settings.get("approot", ".")):
-            if type:
-                # Recursively build the parent service first
-                sources += build_service(type, get_src(type), dest, **settings)
-                # If the parent service has an 'extend' script, run it
-                for src in sources:
-                    execute_script("{src}/extend".format(src=src), home=dest)
-            # Create dest
-            if not os.path.exists(dest):
-                os.makedirs(dest)
-            # Call build script
-            execute_script("./build", home=dest)
-            # Copy execution files
-            if os.path.exists("profile"):
-                copy("profile", dest)
-            if os.path.exists("run"):
-                copy("run", dest)
-    return sources
+    build_app(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
     main()
